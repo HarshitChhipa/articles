090bacb9fb095bac4863b5b975d958a6223363ba
Karan Garg <karan.garg2@gmail.com>
2018-04-15 22:17:40 +0530

    Create usedb.js

---
+++ usedb.js
@@ -0,0 +1,23 @@
+fetch(url)
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    networkDataReceived = true;
+    console.log('From web', data);
+    var dataArray = [];
+    for (var key in data) {
+      dataArray.push(data[key]);
+    }
+    updateUI(dataArray);
+  });
+
+if ('indexedDB' in window) {
+  readAllData('posts')
+    .then(function(data) {
+      if (!networkDataReceived) {
+        console.log('From cache', data);
+        updateUI(data);
+      }
+    });
+}
